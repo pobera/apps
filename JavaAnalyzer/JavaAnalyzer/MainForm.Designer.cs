dbd7ad91c51a36807d6c0c3767f331f50ff10d57
pobera <148545669+pobera@users.noreply.github.com>
2025-11-23 16:09:50 +0500

    Add files via upload

---
+++ JavaAnalyzer/JavaAnalyzer/MainForm.Designer.cs
@@ -0,0 +1,328 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Drawing;
+
+namespace JavaAnalyzer
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private MenuStrip menuStrip;
+        private SplitContainer mainSplitContainer;
+        private SplitContainer leftSplitContainer;
+        private RichTextBox codeTextBox;
+        private TextBox errorsTextBox;
+        private TabControl tabControl;
+        private TabPage tokensTabPage;
+        private TabPage treeTabPage;
+        private DataGridView tokensDataGridView;
+        private TreeView treeView;
+        private Button exitButton;
+        private Panel buttonsPanel;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.leftSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.codeTextBox = new System.Windows.Forms.RichTextBox();
+            this.errorsTextBox = new System.Windows.Forms.TextBox();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tokensTabPage = new System.Windows.Forms.TabPage();
+            this.tokensDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.treeTabPage = new System.Windows.Forms.TabPage();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.buttonsPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
+            this.mainSplitContainer.Panel1.SuspendLayout();
+            this.mainSplitContainer.Panel2.SuspendLayout();
+            this.mainSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftSplitContainer)).BeginInit();
+            this.leftSplitContainer.Panel1.SuspendLayout();
+            this.leftSplitContainer.Panel2.SuspendLayout();
+            this.leftSplitContainer.SuspendLayout();
+            this.tabControl.SuspendLayout();
+            this.tokensTabPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tokensDataGridView)).BeginInit();
+            this.treeTabPage.SuspendLayout();
+            this.buttonsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1321, 24);
+            this.menuStrip.TabIndex = 3;
+            // 
+            // mainSplitContainer
+            // 
+            this.mainSplitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainSplitContainer.BackColor = System.Drawing.SystemColors.Control;
+            this.mainSplitContainer.Location = new System.Drawing.Point(0, 28);
+            this.mainSplitContainer.Name = "mainSplitContainer";
+            // 
+            // mainSplitContainer.Panel1
+            // 
+            this.mainSplitContainer.Panel1.BackColor = System.Drawing.Color.White;
+            this.mainSplitContainer.Panel1.Controls.Add(this.leftSplitContainer);
+            // 
+            // mainSplitContainer.Panel2
+            // 
+            this.mainSplitContainer.Panel2.BackColor = System.Drawing.Color.White;
+            this.mainSplitContainer.Panel2.Controls.Add(this.tabControl);
+            this.mainSplitContainer.Size = new System.Drawing.Size(1321, 589);
+            this.mainSplitContainer.SplitterDistance = 748;
+            this.mainSplitContainer.SplitterWidth = 5;
+            this.mainSplitContainer.TabIndex = 0;
+            // 
+            // leftSplitContainer
+            // 
+            this.leftSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.leftSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.leftSplitContainer.Name = "leftSplitContainer";
+            this.leftSplitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // leftSplitContainer.Panel1
+            // 
+            this.leftSplitContainer.Panel1.Controls.Add(this.codeTextBox);
+            // 
+            // leftSplitContainer.Panel2
+            // 
+            this.leftSplitContainer.Panel2.Controls.Add(this.errorsTextBox);
+            this.leftSplitContainer.Size = new System.Drawing.Size(748, 589);
+            this.leftSplitContainer.SplitterDistance = 468;
+            this.leftSplitContainer.SplitterWidth = 5;
+            this.leftSplitContainer.TabIndex = 0;
+            // 
+            // codeTextBox
+            // 
+            this.codeTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.codeTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.codeTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.codeTextBox.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.codeTextBox.Location = new System.Drawing.Point(0, 0);
+            this.codeTextBox.Name = "codeTextBox";
+            this.codeTextBox.Size = new System.Drawing.Size(748, 468);
+            this.codeTextBox.TabIndex = 0;
+            this.codeTextBox.Text = "";
+            // 
+            // errorsTextBox
+            // 
+            this.errorsTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.errorsTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.errorsTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.errorsTextBox.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.errorsTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.errorsTextBox.Location = new System.Drawing.Point(0, 0);
+            this.errorsTextBox.Multiline = true;
+            this.errorsTextBox.Name = "errorsTextBox";
+            this.errorsTextBox.ReadOnly = true;
+            this.errorsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.errorsTextBox.Size = new System.Drawing.Size(748, 116);
+            this.errorsTextBox.TabIndex = 0;
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tokensTabPage);
+            this.tabControl.Controls.Add(this.treeTabPage);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(568, 589);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tokensTabPage
+            // 
+            this.tokensTabPage.Controls.Add(this.tokensDataGridView);
+            this.tokensTabPage.Location = new System.Drawing.Point(4, 24);
+            this.tokensTabPage.Name = "tokensTabPage";
+            this.tokensTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.tokensTabPage.Size = new System.Drawing.Size(560, 561);
+            this.tokensTabPage.TabIndex = 0;
+            this.tokensTabPage.Text = "Таблица токенов";
+            // 
+            // tokensDataGridView
+            // 
+            this.tokensDataGridView.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.tokensDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tokensDataGridView.ColumnHeadersHeight = 29;
+            this.tokensDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.tokensDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.tokensDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tokensDataGridView.GridColor = System.Drawing.SystemColors.ControlLight;
+            this.tokensDataGridView.Location = new System.Drawing.Point(3, 3);
+            this.tokensDataGridView.Name = "tokensDataGridView";
+            this.tokensDataGridView.RowHeadersWidth = 51;
+            this.tokensDataGridView.RowTemplate.Height = 24;
+            this.tokensDataGridView.Size = new System.Drawing.Size(554, 555);
+            this.tokensDataGridView.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Type";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Position";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Length";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 125;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "Text";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 6;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 125;
+            // 
+            // treeTabPage
+            // 
+            this.treeTabPage.Controls.Add(this.treeView);
+            this.treeTabPage.Location = new System.Drawing.Point(4, 24);
+            this.treeTabPage.Name = "treeTabPage";
+            this.treeTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.treeTabPage.Size = new System.Drawing.Size(560, 561);
+            this.treeTabPage.TabIndex = 1;
+            this.treeTabPage.Text = "Дерево";
+            // 
+            // treeView
+            // 
+            this.treeView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.treeView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.Font = new System.Drawing.Font("Segoe UI", 13F);
+            this.treeView.Location = new System.Drawing.Point(3, 3);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(554, 555);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Location = new System.Drawing.Point(1066, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(251, 51);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Выход";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // buttonsPanel
+            // 
+            this.buttonsPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.buttonsPanel.Controls.Add(this.button1);
+            this.buttonsPanel.Controls.Add(this.updateButton);
+            this.buttonsPanel.Controls.Add(this.exitButton);
+            this.buttonsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonsPanel.Location = new System.Drawing.Point(0, 617);
+            this.buttonsPanel.Name = "buttonsPanel";
+            this.buttonsPanel.Size = new System.Drawing.Size(1321, 51);
+            this.buttonsPanel.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(509, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 51);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Загрузить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.updateButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(230)))), ((int)(((byte)(230)))));
+            this.updateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.updateButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.updateButton.Location = new System.Drawing.Point(752, 0);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(251, 51);
+            this.updateButton.TabIndex = 1;
+            this.updateButton.Text = "Анализировать";
+            this.updateButton.UseVisualStyleBackColor = false;
+            this.updateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1321, 668);
+            this.Controls.Add(this.menuStrip);
+            this.Controls.Add(this.buttonsPanel);
+            this.Controls.Add(this.mainSplitContainer);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Анализатор для языка Java";
+            this.mainSplitContainer.Panel1.ResumeLayout(false);
+            this.mainSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).EndInit();
+            this.mainSplitContainer.ResumeLayout(false);
+            this.leftSplitContainer.Panel1.ResumeLayout(false);
+            this.leftSplitContainer.Panel2.ResumeLayout(false);
+            this.leftSplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftSplitContainer)).EndInit();
+            this.leftSplitContainer.ResumeLayout(false);
+            this.tabControl.ResumeLayout(false);
+            this.tokensTabPage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tokensDataGridView)).EndInit();
+            this.treeTabPage.ResumeLayout(false);
+            this.buttonsPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private Button button1;
+        private Button updateButton;
+    }
+}
---
+++ JavaAnalyzer/JavaAnalyzer/MainForm.cs
@@ -0,0 +1,224 @@
+﻿using Antlr4.Runtime;
+using JavaAnalyzer;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using Antlr4.Runtime.Tree;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+
+namespace JavaAnalyzer
+{
+    public partial class MainForm : Form
+    {
+        string filePath = "";
+        private string _codeContent = string.Empty;
+        private List<string> _codeLines = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeDataGridViewColumns();
+            ConfigureDataGridViewLayout();
+            LoadTokensAndTree();
+        }
+
+        private void InitializeDataGridViewColumns()
+        {
+            tokensDataGridView.Columns.Clear();
+
+            // Настраиваем колонки таблицы
+            tokensDataGridView.Columns.Add("Type", "Тип токена");
+            tokensDataGridView.Columns.Add("Line", "Строка");
+            tokensDataGridView.Columns.Add("Position", "Позиция");
+            tokensDataGridView.Columns.Add("Text", "Текст");
+
+            // Настраиваем ширину колонок
+            tokensDataGridView.Columns["Type"].Width = 150;
+            tokensDataGridView.Columns["Line"].Width = 60;
+            tokensDataGridView.Columns["Position"].Width = 70;
+            tokensDataGridView.Columns["Text"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void ConfigureDataGridViewLayout()
+        {
+            // Настраиваем внешний вид таблицы
+            tokensDataGridView.Dock = DockStyle.Fill;
+            tokensDataGridView.ReadOnly = true;
+            tokensDataGridView.RowHeadersVisible = false;
+            tokensDataGridView.ScrollBars = ScrollBars.Vertical;
+            tokensDataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            tokensDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+            tokensDataGridView.AllowUserToResizeColumns = false;
+            tokensDataGridView.AllowUserToResizeRows = false;
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            LoadTokensAndTree();
+        }
+
+        private void LoadTokensAndTree()
+        {
+            try
+            {
+                _codeContent = codeTextBox.Text;
+                _codeLines = _codeContent.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None).ToList();
+
+                var inputStream = new AntlrInputStream(_codeContent);
+                var lexer = new JavaLexer(inputStream);
+                var tokenStream = new CommonTokenStream(lexer);
+
+                var parser = new JavaParser(tokenStream);
+                var errorListener = new ErrorListener();
+                parser.AddErrorListener(errorListener);
+
+                tokenStream.Fill();
+                tokensDataGridView.Rows.Clear();
+
+                foreach (var token in tokenStream.GetTokens())
+                {
+                    if (token.Type == JavaLexer.Eof || token.Type == JavaLexer.WS)
+                        continue;
+
+                    // Получаем номер строки и позицию
+                    int line = token.Line;
+                    int position = token.Column + 1; // ANTLR использует 0-based позиции
+
+                    tokensDataGridView.Rows.Add(
+                        lexer.Vocabulary.GetSymbolicName(token.Type),
+                        line,
+                        position,
+                        token.Text);
+                }
+
+                var tree = parser.compilationUnit();
+                if (errorListener.Errors.Count > 0)
+                {
+                    errorsTextBox.Text = string.Join(Environment.NewLine, errorListener.Errors);
+                    treeView.Nodes.Clear();
+                    return;
+                }
+                else if (codeTextBox.Text.Length > 0 && errorListener.Errors.Count == 0)
+                {
+                    errorsTextBox.Text = "Синтаксических ошибок не обнаружено.";
+                }
+
+                DisplayParseTree(tree, parser);
+            }
+            catch (Exception ex)
+            {
+                errorsTextBox.Text = $"Ошибка при обработке файла: {ex.Message}";
+            }
+        }
+
+        private void DisplayParseTree(IParseTree tree, JavaParser parser)
+        {
+            if (treeView.InvokeRequired)
+            {
+                treeView.Invoke(new Action(() => DisplayParseTree(tree, parser)));
+                return;
+            }
+
+            treeView.BeginUpdate();
+            try
+            {
+                treeView.Nodes.Clear();
+                AddTreeNodes(treeView.Nodes, tree, parser);
+            }
+            finally
+            {
+                treeView.EndUpdate();
+            }
+        }
+
+        private void AddTreeNodes(TreeNodeCollection nodes, IParseTree tree, JavaParser parser)
+        {
+            string nodeText;
+            int startIndex = -1;
+            int stopIndex = -1;
+
+            if (tree is ITerminalNode terminal)
+            {
+                nodeText = $"{terminal.Symbol.Text} ({parser.Vocabulary.GetSymbolicName(terminal.Symbol.Type)})";
+                startIndex = terminal.Symbol.StartIndex;
+                stopIndex = terminal.Symbol.StopIndex;
+            }
+            else if (tree is ParserRuleContext rule)
+            {
+                nodeText = parser.RuleNames[rule.RuleIndex];
+                startIndex = rule.Start.StartIndex;
+                stopIndex = rule.Stop?.StopIndex ?? rule.Start.StopIndex;
+            }
+            else
+            {
+                nodeText = "Unknown";
+            }
+
+            var node = nodes.Add(nodeText);
+            node.Tag = new Tuple<int, int>(startIndex, stopIndex);
+
+            for (int i = 0; i < tree.ChildCount; i++)
+            {
+                AddTreeNodes(node.Nodes, tree.GetChild(i), parser);
+            }
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node?.Tag is Tuple<int, int> positions)
+            {
+                int start = positions.Item1;
+                int end = positions.Item2;
+
+                if (start >= 0 && end >= 0 && end >= start)
+                {
+                    codeTextBox.BeginInvoke((Action)(() =>
+                    {
+                        codeTextBox.SelectionStart = start;
+                        codeTextBox.SelectionLength = end - start + 1;
+                        codeTextBox.ScrollToCaret();
+                        codeTextBox.Focus();
+                    }));
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var ofd = new OpenFileDialog())
+                {
+                    ofd.Filter = "Java files (*.java)|*.java|All files (*.*)|*.*";
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        filePath = ofd.FileName;
+                        codeTextBox.Text = File.ReadAllText(filePath);
+                        LoadTokensAndTree();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorsTextBox.Text = $"Ошибка загрузки: {ex.Message}";
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e) => Application.Exit();
+    }
+
+    public class ErrorListener : BaseErrorListener
+    {
+        public List<string> Errors { get; } = new List<string>();
+
+        public override void SyntaxError(TextWriter output, IRecognizer recognizer,
+            IToken offendingSymbol, int line, int charPositionInLine,
+            string msg, RecognitionException e)
+        {
+            Errors.Add($"Строка {line}:{charPositionInLine + 1} - {msg}");
+        }
+    }
+}
